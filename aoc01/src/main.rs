73e11d24a171226b0b68262db5b0709a5a4ebd7b
livexia <istillalive@live.cn>
2022-12-01 13:24:57 +0800

    finish part1

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -7,11 +7,37 @@ macro_rules! err {
 
 type Result<T> = ::std::result::Result<T, Box<dyn Error>>;
 
-fn main() -> Result<()>{
+fn main() -> Result<()> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
+    let mut elves_calories = Vec::new();
+    let mut calories: Vec<i32> = vec![];
+    for line in input.lines() {
+        if line.is_empty() {
+            if !calories.is_empty() {
+                elves_calories.push(calories.clone());
+                calories.clear();
+            }
+        } else {
+            calories.push(line.parse().unwrap());
+        }
+    }
 
-    // part1()?;
+    part1(&elves_calories)?;
     // part2()?;
+    Ok(())
+}
+
+fn part1(elves_calories: &[Vec<i32>]) -> Result<()> {
+    let max_calories = elves_calories
+        .iter()
+        .map(|c| c.iter().sum::<i32>())
+        .max()
+        .unwrap();
+    writeln!(
+        io::stdout(),
+        "How many total Calories is that Elf carrying?: {}",
+        max_calories
+    )?;
     Ok(())
 }
