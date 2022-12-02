8c7b881b1cf06c02d36f8b00c89e85390dd66baf
livexia <istillalive@live.cn>
2022-12-02 13:39:47 +0800

    finish part2

--- aoc02/src/main.rs
+++ aoc02/src/main.rs
@@ -12,19 +12,44 @@ type Result<T> = ::std::result::Result<T, Box<dyn Error>>;
 fn main() -> Result<()> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
+
+    part1(&input)?;
+    part2(&input)?;
+    Ok(())
+}
+
+fn part1(input: &str) -> Result<()> {
     let rounds: Vec<(Shape, Shape)> = input
         .lines()
         .map(|l| l.split_once(' ').unwrap())
         .map(|(f, l)| (f.parse().unwrap(), l.parse().unwrap()))
         .collect();
-
-    part1(&rounds)?;
-    // part2()?;
+    let score = rounds.iter().fold(0, |score, &(a, b)| (b - a) + score);
+    writeln!(io::stdout(), "What would your total score be if everything goes exactly according to your strategy guide? {}", score)?;
     Ok(())
 }
 
-fn part1(rounds: &[(Shape, Shape)]) -> Result<()> {
-    let score = rounds.iter().fold(0, |score, &(a, b)| (b - a) + score);
+fn part2(input: &str) -> Result<()> {
+    use Outcome::*;
+    use Shape::*;
+    let score = input
+        .lines()
+        .map(|l| l.split_once(' ').unwrap())
+        .map(|(f, l)| (f.parse::<Shape>().unwrap(), l.parse::<Outcome>().unwrap()))
+        .fold(0, |score, (shape, outcome)| {
+            score
+                + match (shape, outcome) {
+                    (Rock, Lose) => 3 + 0,
+                    (Rock, Draw) => 1 + 3,
+                    (Rock, Win) => 2 + 6,
+                    (Paper, Lose) => 1 + 0,
+                    (Paper, Draw) => 2 + 3,
+                    (Paper, Win) => 3 + 6,
+                    (Scissors, Lose) => 2 + 0,
+                    (Scissors, Draw) => 3 + 3,
+                    (Scissors, Win) => 1 + 6,
+                }
+        });
     writeln!(io::stdout(), "What would your total score be if everything goes exactly according to your strategy guide? {}", score)?;
     Ok(())
 }
@@ -80,5 +105,31 @@ impl Sub for Shape {
                 (Scissors, Paper) => 6,
                 (Scissors, Scissors) => 3,
             }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Outcome {
+    Lose,
+    Draw,
+    Win,
+}
+
+impl FromStr for Outcome {
+    type Err = Box<dyn Error>;
+
+    fn from_str(s: &str) -> Result<Self> {
+        use Outcome::*;
+
+        if s.len() != 1 {
+            err!("{:?} is not a outcome", s)
+        } else {
+            Ok(match s {
+                "X" => Lose,
+                "Y" => Draw,
+                "Z" => Win,
+                _ => return err!("{:?} is not a outcome", s),
+            })
+        }
     }
 }
