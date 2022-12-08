236c0a6cc0790e3770d2206adf356aefbb39510a
livexia <istillalive@live.cn>
2022-12-08 16:40:37 +0800

    pass day 8

--- aoc08/src/main.rs
+++ aoc08/src/main.rs
@@ -1,6 +1,7 @@
 use std::collections::{HashMap, HashSet};
 use std::error::Error;
 use std::io::{self, Read, Write};
+use std::vec;
 
 macro_rules! err {
     ($($tt:tt)*) => { Err(Box::<dyn Error>::from(format!($($tt)*))) }
@@ -11,150 +12,161 @@ type Result<T> = ::std::result::Result<T, Box<dyn Error>>;
 fn main() -> Result<()> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
-    let input: Vec<_> = input.lines().collect();
-    let map: HashMap<(i32, i32), u8> = input
-        .iter()
-        .enumerate()
-        .map(|(x, l)| {
-            l.bytes()
-                .enumerate()
-                .map(|(y, b)| ((x as i32, y as i32), b - b'0'))
-                .collect::<Vec<_>>()
-        })
-        .flatten()
+    let map: Vec<Vec<_>> = input
+        .lines()
+        .map(|l| l.bytes().map(|b| b - b'0').collect())
         .collect();
+    part1(&map)?;
+    part2(&map)?;
+    Ok(())
+}
 
-    let height = input.len() as i32; // x
-    let width = input[0].len() as i32; // y
+fn visible_at(
+    p: (usize, usize),
+    map: &[Vec<u8>],
+    bound: usize,
+    move_fn: fn(usize, usize, usize) -> (usize, usize),
+) -> (usize, usize) {
+    let mut pp = p;
+    let mut np = move_fn(pp.0, pp.1, bound);
+    while pp != np && !is_on_edge(pp, map.len(), map[0].len()) {
+        if map[p.0][p.1] > map[np.0][np.1] {
+            pp = np;
+            np = move_fn(np.0, np.1, bound);
+        } else {
+            break;
+        }
+    }
+    pp
+}
 
-    use Direction::*;
+fn is_on_edge(p: (usize, usize), height: usize, width: usize) -> bool {
+    p.0 == 0 || p.1 == 0 || p.0 == height - 1 || p.1 == width - 1
+}
 
-    let mut left_visible = HashMap::new();
-    let mut right_visible = HashMap::new();
-    let mut top_visible = HashMap::new();
-    let mut bottom_visible = HashMap::new();
-    let mut visible = HashSet::new();
+fn move_left(x: usize, y: usize, bound: usize) -> (usize, usize) {
+    if y > bound {
+        (x, y - 1)
+    } else {
+        (x, y)
+    }
+}
+
+fn move_right(x: usize, y: usize, bound: usize) -> (usize, usize) {
+    if y + 1 < bound {
+        (x, y + 1)
+    } else {
+        (x, y)
+    }
+}
+
+fn move_up(x: usize, y: usize, bound: usize) -> (usize, usize) {
+    if x > bound {
+        (x - 1, y)
+    } else {
+        (x, y)
+    }
+}
+
+fn move_down(x: usize, y: usize, bound: usize) -> (usize, usize) {
+    if x + 1 < bound {
+        (x + 1, y)
+    } else {
+        (x, y)
+    }
+}
+
+fn distance(p1: (usize, usize), p2: (usize, usize)) -> usize {
+    (((p1.0 as i32) - (p2.0 as i32)).abs() + ((p1.1 as i32) - (p2.1 as i32)).abs()) as usize
+}
+
+fn part1(map: &[Vec<u8>]) -> Result<()> {
+    let height = map.len();
+    let width = map[0].len();
+
+    let mut visible = vec![vec![false; width]; height];
+
     for i in 0..height {
-        left_visible.insert((i, 0), (map.get(&(i, 0)).unwrap(), 0));
-        right_visible.insert((i, width - 1), (map.get(&(i, width - 1)).unwrap(), 0));
-        visible.insert((i, 0));
-        visible.insert((i, width - 1));
+        for j in 0..width {
+            if !visible[i][j] && is_on_edge(visible_at((i, j), &map, 0, move_left), height, width) {
+                visible[i][j] = true;
+            }
+            if !visible[i][j]
+                && is_on_edge(visible_at((i, j), &map, width, move_right), height, width)
+            {
+                visible[i][j] = true;
+            }
+            if !visible[i][j] && is_on_edge(visible_at((i, j), &map, 0, move_up), height, width) {
+                visible[i][j] = true;
+            }
+            if !visible[i][j]
+                && is_on_edge(visible_at((i, j), &map, height, move_down), height, width)
+            {
+                visible[i][j] = true;
+            }
+        }
+    }
+    let count = visible.iter().flatten().filter(|&&b| b).count();
+    writeln!(
+        io::stdout(),
+        "how many trees are visible from outside the grid? {}",
+        count
+    )?;
+
+    Ok(())
+}
+
+fn part2(map: &[Vec<u8>]) -> Result<()> {
+    let height = map.len();
+    let width = map[0].len();
+
+    let mut distances = vec![vec![1; width]; height];
+    for i in 0..height {
+        distances[i][0] = 0;
+        distances[i][width - 1] = 0;
     }
     for j in 0..width {
-        top_visible.insert((0, j), (map.get(&(0, j)).unwrap(), 0));
-        bottom_visible.insert((height - 1, j), (map.get(&(height - 1, j)).unwrap(), 0));
-        visible.insert((0, j));
-        visible.insert((height - 1, j));
-    }
-
-    for x in 0..height {
-        for y in 0..width {
-            let h = map.get(&(x, y)).unwrap();
-            for (d, v) in [(Left, &mut left_visible), (Up, &mut top_visible)] {
-                if v.contains_key(&(x, y)) {
-                    continue;
-                }
-                let p = d.next(x, y);
-                let ph = map.get(&p).unwrap();
-                if let Some(&(vh, count)) = v.get(&p) {
-                    let new_count = if h > ph { count + 1 } else { 1 };
-                    if vh < h {
-                        visible.insert((x, y));
-                        v.insert((x, y), (h, new_count));
-                    } else {
-                        v.insert((x, y), (vh, new_count));
-                    }
-                } else {
-                    unreachable!("{:?} {:?}", (x, y), d);
-                }
-            }
-        }
-    }
-
-    for x in (0..height).rev() {
-        for y in (0..width).rev() {
-            let h = map.get(&(x, y)).unwrap();
-            for (d, v) in [(Right, &mut right_visible), (Down, &mut bottom_visible)] {
-                if v.contains_key(&(x, y)) {
-                    continue;
-                }
-                let p = d.next(x, y);
-                let ph = map.get(&p).unwrap();
-                if let Some(&(vh, count)) = v.get(&p) {
-                    let new_count = if h > ph { count + 1 } else { 1 };
-                    if vh < h {
-                        visible.insert((x, y));
-                        v.insert((x, y), (h, new_count));
-                    } else {
-                        v.insert((x, y), (vh, new_count));
-                    }
-                } else {
-                    unreachable!("{:?} {:?}", (x, y), d);
-                }
-            }
-        }
+        distances[0][j] = 0;
+        distances[height - 1][j] = 0;
     }
 
     for i in 0..height {
         for j in 0..width {
-            if visible.contains(&(i, j)) {
-                print!("~");
+            let p = visible_at((i, j), &map, 0, move_left);
+            if is_on_edge(p, height, width) {
+                distances[i][j] *= distance(p, (i, j))
             } else {
-                print!("x");
+                distances[i][j] *= distance(p, (i, j)) + 1;
+            }
+
+            let p = visible_at((i, j), &map, width, move_right);
+            if is_on_edge(p, height, width) {
+                distances[i][j] *= distance(p, (i, j))
+            } else {
+                distances[i][j] *= distance(p, (i, j)) + 1;
+            }
+
+            let p = visible_at((i, j), &map, 0, move_up);
+            if is_on_edge(p, height, width) {
+                distances[i][j] *= distance(p, (i, j))
+            } else {
+                distances[i][j] *= distance(p, (i, j)) + 1;
+            }
+
+            let p = visible_at((i, j), &map, height, move_down);
+            if is_on_edge(p, height, width) {
+                distances[i][j] *= distance(p, (i, j))
+            } else {
+                distances[i][j] *= distance(p, (i, j)) + 1;
             }
         }
-        println!();
     }
+    let max_distance = distances.iter().flatten().max().unwrap();
 
-    dbg!(visible.len());
-
-    let mut max_distance = 0;
-    for x in 0..height {
-        for y in 0..width {
-            let mut distance = 1;
-            let p = (x, y);
-            for v in [&left_visible, &right_visible, &top_visible, &bottom_visible] {
-                if let Some(&(_, count)) = v.get(&p) {
-                    distance *= count;
-                }
-            }
-            if p == (1, 3) {
-                dbg!(p);
-                dbg!(map.get(&p));
-                dbg!(left_visible.get(&p).unwrap().1);
-                dbg!(top_visible.get(&p).unwrap().1);
-                dbg!(right_visible.get(&p).unwrap().1);
-                dbg!(bottom_visible.get(&p).unwrap().1);
-            }
-            max_distance = distance.max(max_distance);
-        }
-    }
-    dbg!(max_distance);
-    part1()?;
-    // part2()?;
-    Ok(())
-}
-
-#[derive(Debug, PartialEq, PartialOrd)]
-enum Direction {
-    Left,
-    Right,
-    Up,
-    Down,
-}
-
-impl Direction {
-    fn next(&self, x: i32, y: i32) -> (i32, i32) {
-        use self::*;
-        match self {
-            Direction::Left => (x, y - 1),
-            Direction::Right => (x, y + 1),
-            Direction::Up => (x - 1, y),
-            Direction::Down => (x + 1, y),
-        }
-    }
-}
-
-fn part1() -> Result<()> {
+    writeln!(
+        io::stdout(),
+        "What is the highest scenic score possible for any tree? {}",
+        max_distance
+    )?;
     Ok(())
 }
